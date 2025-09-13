f711e8a02392e34f989020bfbc49ba5a4557a581
Udhay2608 <U-Tiwana23@st-bernards.slough.sch.uk>
2025-09-13 08:56:41 +0100

    Add files via upload

--- hw.py
+++ hw.py
@@ -1,9 +1,4 @@
-num=int(input("Enter a number"))
-if num == 0:
-    count = 1
-else:
-    count=0
-while n >0:
-    n //=10
-    count += 1
-print("number of digits in", num, "is",count)
+rows=int(input("Enter the number of rows "))
+for i in range(1, rows + 1):
+    print(""*(rows-1), end="")
+    print("*" * 1)
