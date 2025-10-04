b92bd1ec7b8e18bfcb82fd2bb1d141ddf5747066
Udhay2608 <U-Tiwana23@st-bernards.slough.sch.uk>
2025-10-04 09:19:03 +0100

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,14 @@
+for x in range(10):
+   if x % 2==0:
+      print("Twist")
+
+elif x % 7==0:
+pass
+elif x % 5==0:
+    print("fizz")
+
+elif  x % 3 == 0:
+     print("buzz")
+
+else:
+    print(x)
